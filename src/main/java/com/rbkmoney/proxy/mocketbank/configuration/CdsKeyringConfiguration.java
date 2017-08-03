ca786e31122cc77c059abb7024f13c451c5aca88
avcherkasov <shnurok14@ya.ru>
2017-08-03 15:43:24 +0300

    PROX-86: modified method cancelled

--- src/main/java/com/rbkmoney/proxy/mocketbank/configuration/CdsKeyringConfiguration.java
+++ src/main/java/com/rbkmoney/proxy/mocketbank/configuration/CdsKeyringConfiguration.java
@@ -1,14 +1,8 @@
 package com.rbkmoney.proxy.mocketbank.configuration;
 
 import com.rbkmoney.damsel.cds.KeyringSrv;
-import com.rbkmoney.woody.api.ClientBuilder;
-import com.rbkmoney.woody.api.event.ClientEventListener;
-import com.rbkmoney.woody.api.event.CompositeClientEventListener;
 import com.rbkmoney.woody.thrift.impl.http.THSpawnClientBuilder;
-import com.rbkmoney.woody.thrift.impl.http.event.ClientEventLogListener;
-import com.rbkmoney.woody.thrift.impl.http.event.HttpClientEventLogListener;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
@@ -22,24 +16,10 @@ public class CdsKeyringConfiguration {
     private Resource resource;
 
     @Bean
-    public KeyringSrv.Iface keyringSrv(ClientEventListener listenerSrv) throws IOException {
-        return clientBuilder()
-                .withEventListener(listenerSrv)
+    public KeyringSrv.Iface keyringSrv() throws IOException {
+        return new THSpawnClientBuilder()
                 .withAddress(resource.getURI())
                 .build(KeyringSrv.Iface.class);
     }
 
-    @Bean
-    @ConditionalOnMissingBean
-    public ClientEventListener listenerSrv() {
-        return new CompositeClientEventListener(
-                new ClientEventLogListener(),
-                new HttpClientEventLogListener()
-        );
-    }
-
-    @Bean(name = "clientBuilderKeyring")
-    public ClientBuilder clientBuilder() {
-        return new THSpawnClientBuilder();
-    }
 }
