dd2c202c06fa653fb210131fec05a5c937344f8b
avcherkasov <shnurok14@ya.ru>
2019-07-25 17:22:11 +0300

    PROX-336: bump date, add validate

--- src/main/java/com/rbkmoney/proxy/mocketbank/handler/p2p/withdrawal/WithdrawalHandler.java
+++ src/main/java/com/rbkmoney/proxy/mocketbank/handler/p2p/withdrawal/WithdrawalHandler.java
@@ -1,5 +1,6 @@
 package com.rbkmoney.proxy.mocketbank.handler.p2p.withdrawal;
 
+import com.rbkmoney.damsel.domain.Currency;
 import com.rbkmoney.damsel.msgpack.Value;
 import com.rbkmoney.damsel.withdrawals.provider_adapter.*;
 import com.rbkmoney.proxy.mocketbank.utils.damsel.withdrawals.WithdrawalsDomainWrapper;
@@ -10,9 +11,11 @@ import org.apache.thrift.TException;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Map;
 
 import static com.rbkmoney.proxy.mocketbank.utils.mocketbank.DateTimeUtils.getCurrentDateTimeByPattern;
+import static com.rbkmoney.proxy.mocketbank.utils.withdrawal.CurrencyConverter.*;
 
 
 @Slf4j
@@ -32,21 +35,28 @@ public class WithdrawalHandler {
     }
 
     public Quote getQuote(GetQuoteParams getQuoteParams, Map<String, String> options) {
+
+        validateRequest(getQuoteParams);
+
         Quote quote = new Quote();
 
         Cash cashFrom = new Cash();
+        Currency currencyFrom = getQuoteParams.getCurrencyFrom();
+        cashFrom.setCurrency(currencyFrom);
+
         Cash cashTo = new Cash();
+        Currency currencyTo = getQuoteParams.getCurrencyTo();
+        cashTo.setCurrency(currencyTo);
 
         // Crypto currency or not? How check?
-        cashFrom.setAmount(getQuoteParams.getExchangeCash().getAmount());
-        cashTo.setAmount(getQuoteParams.getExchangeCash().getAmount() * 2);
-        if (isExchangeCurrencyTo(getQuoteParams)) {
-            cashFrom.setCurrency(getQuoteParams.getCurrencyFrom());
-            cashTo.setCurrency(getQuoteParams.getCurrencyTo());
+        if (isCryptoCurrency(getQuoteParams.getCurrencyTo())) {
+            cashFrom.setAmount(getQuoteParams.getExchangeCash().getAmount());
+            cashTo.setAmount(getQuoteParams.getExchangeCash().getAmount() * 2);
         } else {
-            cashTo.setCurrency(getQuoteParams.getCurrencyFrom());
-            cashFrom.setCurrency(getQuoteParams.getCurrencyTo());
+            cashFrom.setAmount(getQuoteParams.getExchangeCash().getAmount() * 2);
+            cashTo.setAmount(getQuoteParams.getExchangeCash().getAmount());
         }
+
         quote.setCashTo(cashTo);
         quote.setCashFrom(cashFrom);
 
@@ -54,15 +64,26 @@ public class WithdrawalHandler {
         quoteData.setStr(getQuoteParams.getIdempotencyId());
         quote.setQuoteData(quoteData);
 
-        String date = getCurrentDateTimeByPattern(Instant.EPOCH.toString());
-        quote.setCreatedAt(date);
-        quote.setExpiresOn(date);
+        String createdAt = getCurrentDateTimeByPattern(Instant.EPOCH.toEpochMilli());
+        String expiresOn = getCurrentDateTimeByPattern(Instant.EPOCH.plus(15, ChronoUnit.MINUTES).toEpochMilli());
+        quote.setCreatedAt(createdAt);
+        quote.setExpiresOn(expiresOn);
 
         return quote;
     }
 
-    public static boolean isExchangeCurrencyTo(GetQuoteParams getQuoteParams) {
-        return getQuoteParams.getExchangeCash().getCurrency().getSymbolicCode().equalsIgnoreCase(getQuoteParams.getCurrencyTo().getSymbolicCode());
+    private void validateRequest(GetQuoteParams getQuoteParams) {
+        if (isCryptoCurrency(getQuoteParams.getCurrencyFrom()) && isCryptoCurrency(getQuoteParams.getCurrencyTo())) {
+            throw new RuntimeException("Can't exchange crypto currency to crypto currency");
+        }
+
+        if (isCurrencyEquals(getQuoteParams.getCurrencyFrom(), getQuoteParams.getCurrencyTo())) {
+            throw new RuntimeException("Can't exchange equals currency");
+        }
+
+        if (!isExchangeCurrencyEqualsOtherCurrency(getQuoteParams)) {
+            throw new RuntimeException("Can't exchange equals currency, all currency is different");
+        }
     }
 
 }
