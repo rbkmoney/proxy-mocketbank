357f836a13414965861cb17b9f458d7b40e27ce6
avcherkasov <shnurok14@ya.ru>
2020-03-06 11:48:42 +0300

    PROX-392: fix after review

--- src/main/java/com/rbkmoney/proxy/mocketbank/utils/model/CardAction.java
+++ src/main/java/com/rbkmoney/proxy/mocketbank/utils/model/CardAction.java
@@ -12,9 +12,9 @@ public enum CardAction {
     UNKNOWN("Unknown"),
     UNSUPPORTED_CARD("Unsupported Card"),
     SUCCESS("Success"),
-    THREE_D_SECURE_SUCCESS("3-D Secure Success"),
-    THREE_D_SECURE_FAILURE("3-D Secure Failure"),
-    THREE_D_SECURE_TIMEOUT("3-D Secure Timeout"),
+    SUCCESS_3DS("3-D Secure Success"),
+    FAILURE_3DS("3-D Secure Failure"),
+    TIMEOUT_3DS("3-D Secure Timeout"),
     INSUFFICIENT_FUNDS("Insufficient Funds"),
     INVALID_CARD("Invalid Card"),
     CVV_MATCH_FAIL("CVV Match Fail"),
@@ -26,6 +26,36 @@ public enum CardAction {
     SAMSUNG_PAY_FAILURE("Samsung Pay Failure"),
     SAMSUNG_PAY_SUCCESS("Samsung Pay Success"),
     UNKNOWN_FAILURE("Unknown Failure");
+
+    private static final CardAction[] ENROLLED_CARDS = {
+            FAILURE_3DS,
+            TIMEOUT_3DS,
+            SUCCESS_3DS
+    };
+
+    private static final CardAction[] FAILED_CARDS = {
+            INSUFFICIENT_FUNDS,
+            INVALID_CARD,
+            CVV_MATCH_FAIL,
+            APPLE_PAY_FAILURE,
+            SAMSUNG_PAY_FAILURE,
+            GOOGLE_PAY_FAILURE,
+            EXPIRED_CARD,
+            UNKNOWN_FAILURE
+    };
+
+    private static final CardAction[] SUCCESS_CARDS = {
+            SUCCESS,
+            APPLE_PAY_SUCCESS,
+            GOOGLE_PAY_SUCCESS,
+            SAMSUNG_PAY_SUCCESS
+    };
+
+    private static final CardAction[] MPI_FAILED_CARDS = {
+            FAILURE_3DS,
+            TIMEOUT_3DS,
+            UNKNOWN_FAILURE
+    };
 
     private final String action;
 
@@ -41,57 +71,19 @@ public enum CardAction {
     }
 
     public static boolean isCardEnrolled(CardAction action) {
-        return Arrays.asList(enrolledCard()).contains(action);
-    }
-
-    private static CardAction[] enrolledCard() {
-        return new CardAction[]{
-                THREE_D_SECURE_FAILURE,
-                THREE_D_SECURE_TIMEOUT,
-                THREE_D_SECURE_SUCCESS
-        };
+        return Arrays.asList(ENROLLED_CARDS).contains(action);
     }
 
     public static boolean isCardFailed(CardAction action) {
-        return Arrays.asList(failedCard()).contains(action);
-    }
-
-    private static CardAction[] failedCard() {
-        return new CardAction[]{
-                INSUFFICIENT_FUNDS,
-                INVALID_CARD,
-                CVV_MATCH_FAIL,
-                APPLE_PAY_FAILURE,
-                SAMSUNG_PAY_FAILURE,
-                GOOGLE_PAY_FAILURE,
-                EXPIRED_CARD,
-                UNKNOWN_FAILURE
-        };
+        return Arrays.asList(FAILED_CARDS).contains(action);
     }
 
     public static boolean isCardSuccess(CardAction action) {
-        return Arrays.asList(successCard()).contains(action);
-    }
-
-    private static CardAction[] successCard() {
-        return new CardAction[]{
-                SUCCESS,
-                APPLE_PAY_SUCCESS,
-                GOOGLE_PAY_SUCCESS,
-                SAMSUNG_PAY_SUCCESS
-        };
+        return Arrays.asList(SUCCESS_CARDS).contains(action);
     }
 
     public static boolean isMpiCardFailed(CardAction action) {
-        return Arrays.asList(mpiFailedCard()).contains(action);
-    }
-
-    private static CardAction[] mpiFailedCard() {
-        return new CardAction[]{
-                THREE_D_SECURE_FAILURE,
-                THREE_D_SECURE_TIMEOUT,
-                UNKNOWN_FAILURE
-        };
+        return Arrays.asList(MPI_FAILED_CARDS).contains(action);
     }
 
 }
