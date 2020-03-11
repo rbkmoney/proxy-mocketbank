eeae769fc79cc79b0d8572e09e9c10cbffe37647
avcherkasov <shnurok14@ya.ru>
2020-03-11 17:48:42 +0300

    PROX-393: rename classes, folder

--- src/main/java/com/rbkmoney/proxy/mocketbank/service/oct/constant/CryptoCurrencies.java
+++ src/main/java/com/rbkmoney/proxy/mocketbank/service/oct/constant/CryptoCurrencies.java
@@ -1,4 +1,4 @@
-package com.rbkmoney.proxy.mocketbank.handler.oct.constant;
+package com.rbkmoney.proxy.mocketbank.service.oct.constant;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
